bee6acde316a565abf7f216c95e0878445e985a9
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-10-26 22:49:07 +0600

    added Authentication Middleware

--- server/src/app/middleware/Auth.middleware.ts
+++ server/src/app/middleware/Auth.middleware.ts
@@ -1,34 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { DecodeToken } from "../utils/jwt";
 import env from "../../config/env";
-import { IsActive } from "../module/user/user.interface";
 import AppError from "../errorHelper/errorHelper";
 import httpStatusCode from 'http-status-codes';
 import { JwtPayload } from "jsonwebtoken";
 import User from "../module/user/user.model";
  
-export const CheckAuth = (...restRole: string[]) =>  async (req: Request, res: Response, next: NextFunction) => {
+export const CheckAuth = () =>  async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.headers.authorization;
         const verifyUser =   DecodeToken(token as string, env.JWT_SECRET_CODE) as JwtPayload;
-        const isUserExist = await User.findOne({email: verifyUser.email});
+        const user = await User.findById(verifyUser.userId).select("-password");
 
          // CHECKING
-        if(!isUserExist) 
-                throw new AppError(httpStatusCode.BAD_REQUEST, "User not exist!");
-        if(isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE)
-            throw new AppError(httpStatusCode.BAD_REQUEST, "Can't access, the user might be blocked or inactive");
-        if(isUserExist.isDeleted) 
-            throw new AppError(httpStatusCode.BAD_REQUEST, "Can't access, user is deleted!");
-        if(!verifyUser)
-            throw new AppError(httpStatusCode.BAD_REQUEST, "Unauthorized");
+        if(!user) throw new AppError(httpStatusCode.BAD_REQUEST, "User not exist!");
+        if(!verifyUser) throw new AppError(httpStatusCode.BAD_REQUEST, "Unauthorized");
 
-
-        if(!restRole.includes(verifyUser.role)){
-            throw new AppError(httpStatusCode.BAD_REQUEST, "You are not permitted to access");
-        }
-
-        req.user = verifyUser;
+        req.user = user; // To getting user info
         next();
     } catch (error) {
         next(error);
--- server/src/app/middleware/userValidate.middleware.ts
+++ server/src/app/middleware/userValidate.middleware.ts
@@ -4,8 +4,13 @@ import {  ZodObject } from "zod";
 
 export const validateRequest =  (ZodSchema: ZodObject) => async (req: Request, res: Response, next: NextFunction) => {
     try {
-        req.body = await ZodSchema.parseAsync(req.body);
-        next();
+        if(typeof req.body.data === "string" ) {
+            req.body = JSON.parse(req.body.data);
+            next();
+        }else {
+            req.body = await ZodSchema.parseAsync(req.body);
+            next();
+        }
     } catch (error) {
         next(error);
     }
